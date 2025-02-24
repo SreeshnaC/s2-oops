10b419e80759274c9d0e5b3187c56a7925a74858
SreeshnaC <sreeshnacholayil07@gmail.com>
2025-02-24 10:18:33 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add{
+public static void main(String[] args){
+int x=150;
+System.out.println("x="+x);
+int y=100;
+System.out.println("y="+y);
+int z=x+y;
+System.out.println("summation of x+y="+z);
+}
+}
---
+++ concat_word.java
@@ -0,0 +1,8 @@
+public class concat_word{
+public static void main(String args[]){
+String fword="mariya";
+String lword="joseph";
+System.out.println(fword.concat(lword));
+System.out.println(lword.concat(fword));
+}
+}
